d6df8850944f5dc4843dea12328a3901e13ddc40
Lienard Griebsch <lienard.griebsch@senacor.com>
2022-08-23 15:40:30 +0200

    Added request validation.

--- src/main/java/com/senacor/intermission/newjava/controller/CustomerController.java
+++ src/main/java/com/senacor/intermission/newjava/controller/CustomerController.java
@@ -6,6 +6,7 @@ import com.senacor.intermission.newjava.model.api.ApiCreateCustomer;
 import com.senacor.intermission.newjava.model.api.ApiCustomer;
 import java.util.Collection;
 import java.util.UUID;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,24 +21,28 @@ public class CustomerController {
     private final CustomerHandler customerHandler;
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Valid
     public ResponseEntity<ApiCustomer> createCustomer(@RequestBody ApiCreateCustomer request) {
         ApiCustomer result = customerHandler.createCustomer(request);
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
     @DeleteMapping(value = "/{customerUuid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Valid
     public ResponseEntity<Void> deleteCustomer(@PathVariable(name = "customerUuid") UUID customerUuid) {
         customerHandler.deleteCustomer(customerUuid);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping(value = "/{customerUuid}/accounts", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Valid
     public ResponseEntity<Collection<UUID>> getAllAccounts(@PathVariable(value = "customerUuid") UUID customerUuid) {
         Collection<UUID> result = customerHandler.getAllAccounts(customerUuid);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @PostMapping(value = "/{customerUuid}/accounts", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Valid
     public ResponseEntity<ApiAccount> createAccount(@PathVariable(value = "customerUuid") UUID customerUuid) {
         ApiAccount result = customerHandler.createAccount(customerUuid);
         return new ResponseEntity<>(result, HttpStatus.CREATED);
