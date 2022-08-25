230e101f5692c6f747283a07b16bd731b9960c6c
Wilke <Claas.Wilke@senacor.com>
2022-08-25 13:07:20 +0200

    Executed Async tasks as virtual threads

--- src/main/java/com/senacor/intermission/newjava/config/AsyncConfiguration.java
+++ src/main/java/com/senacor/intermission/newjava/config/AsyncConfiguration.java
@@ -1,9 +1,19 @@
 package com.senacor.intermission.newjava.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableAsync
-public class AsyncConfiguration {
+public class AsyncConfiguration implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return Executors.newVirtualThreadPerTaskExecutor();
+    }
 }
